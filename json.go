1f4fdf0bd2515d4b7e80da38e2397c75884e3bb7
Angel Ramirez <angelra@gmail.com>
2024-11-14 06:40:15 -0800

    fix sec issue

--- json.go
+++ json.go
@@ -27,10 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
-	// _, err = w.Write(dat)
-	// if err != nil {
-	// 	log.Printf("Error sending JSON: %s\n", err)
-	// 	return
-	// }
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error sending JSON: %s\n", err)
+		return
+	}
 }
